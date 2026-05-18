a0d441eb04cb3b33d8af9891e1152002e69d4432
Marcin Lisowski <mnlisows@gmail.com>
2026-05-18 07:41:01 +0200

    feat: enum with generation requirements for methods

---
+++ src/main/java/com/mlisows/testgen/domain/GenerationRequirement.java
@@ -0,0 +1,13 @@
+package com.mlisows.testgen.domain;
+
+public enum GenerationRequirement {
+    NO_ARG_CONSTRUCTOR,
+    PRIMITIVE_ARGUMENT,
+    STRING_ARGUMENT,
+    ENUM_ARGUMENT,
+    OBJECT_FIXTURE,
+    COLLECTION_FIXTURE,
+    MAP_FIXTURE,
+    INTERFACE_MOCK,
+    FRAMEWORK_CONTEXT
+}
