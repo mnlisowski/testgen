5a8abef252b316f886d314b0d133f76679e9ae9e
Marcin Lisowski <mnlisows@gmail.com>
2026-09-04 07:26:56 +0200

    feat: add executable signature helper

---
+++ src/main/java/com/mlisows/testgen/domain/ExecutableSignature.java
@@ -0,0 +1,36 @@
+package com.mlisows.testgen.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class ExecutableSignature {
+    private ExecutableSignature() {
+    }
+
+    public static String method(String className, MethodModel method) {
+        return method(className, method.getName(), parameterTypes(method.getParameters()));
+    }
+
+    public static String method(String className, String methodName, List<String> parameterTypes) {
+        return className + "." + methodName + "(" + join(parameterTypes) + ")";
+    }
+
+    public static String constructor(String className, ConstructorModel constructor) {
+        return constructor(className, parameterTypes(constructor.getParameters()));
+    }
+
+    public static String constructor(String className, List<String> parameterTypes) {
+        return className + ".<init>(" + join(parameterTypes) + ")";
+    }
+
+    private static List<String> parameterTypes(List<ParameterModel> parameters) {
+        return parameters.stream()
+                .map(ParameterModel::getType)
+                .toList();
+    }
+
+    private static String join(List<String> parameterTypes) {
+        return parameterTypes.stream()
+                .collect(Collectors.joining(","));
+    }
+}
